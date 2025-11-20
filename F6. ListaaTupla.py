b14b14abc84d780d2d338289db4a5f9a1dd8864b
estivengalvis-rgb <galvisstivens@gmail.com>
2025-11-20 10:46:04 -0500

    Add files via upload

---
+++ F6. ListaaTupla.py
@@ -0,0 +1,4 @@
+lista=['rojo','azul','rojo','verde']
+seT=set(lista)
+tupla=tuple(seT)
+print(tupla)
---
+++ F7. CoordenadaRango.py
@@ -0,0 +1,8 @@
+coo=(4, 6)
+a=0
+b=10
+x, y=coo
+if a<=x<=b and a<=y<=b:
+    print("Dentro del rango")
+else:
+    print("Fuera del rango")
